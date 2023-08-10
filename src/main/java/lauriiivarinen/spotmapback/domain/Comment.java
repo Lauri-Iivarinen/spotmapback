67527b91f21efd54b049fac33bfe236b8b5ca3f9
Lauri Iivarinen <lauri.iivarinen@gmail.com>
2023-08-10 12:29:11 +0300

    deploy to heroku 0.2

--- src/main/java/lauriiivarinen/spotmapback/domain/Comment.java
+++ src/main/java/lauriiivarinen/spotmapback/domain/Comment.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
+//Comment spots
 @Entity(name="comments")
 public class Comment {
 	
